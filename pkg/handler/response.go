a4cb29202c1f0ed1832a2a53ec742b57bb203176
tsarkovmi <tsarkovmichael@gmail.com>
2024-10-21 17:54:04 +0300

    комент на response.go

--- pkg/handler/response.go
+++ pkg/handler/response.go
@@ -9,9 +9,11 @@ type errorResponse struct {
 	Message string `json:"message"`
 }
 
+/*
 type statusResponse struct {
 	Status string `json:"status"`
 }
+*/
 
 func newErrorResponse(c *gin.Context, statusCode int, message string) {
 	logrus.Error(message)
